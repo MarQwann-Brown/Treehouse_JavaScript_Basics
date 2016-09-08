cdd94f36ae4d9de5889ba7e75d73ad0ad86c66c4
MarQwann Brown <marqwann@gmail.com>
2016-09-08 00:42:12 -0400

    Reviewing HTML Special Chars

---
+++ Stage_02/video05/scripts.js
@@ -0,0 +1,13 @@
+var message = "hello";
+alert(message);
+message = "Goodbye";
+alert(message);
+message = "she's a great person";
+alert(message);
+var htmlSnippet = '<h1 class="special">Important Headline</h1>';
+var message = 'she\'s a great person';
+alert(message);
+htmlSnippet = "<h1 class=\"special\">Important Headline</h1>";
+document.write(htmlSnippet);
+htmlSnippet = "&lth1 class=\"special\"&gtImportant Headline&lt/h1&gt";
+document.write(htmlSnippet);
